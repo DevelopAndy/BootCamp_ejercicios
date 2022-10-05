c47365b8e966b4740d06cd442228e424d65458c9
Andy <ander1386@gmail.com>
2022-10-05 09:09:47 -0500

    first exercise

---
+++ src/main/java/Ejercicios/Ejercicio_1.java
@@ -0,0 +1,47 @@
+package Ejercicios;
+
+public class Ejercicio_1 {
+
+    /*
+    Primera parte:
+
+    Crear una función con tres parámetros que sean números que se suman entre sí.
+
+    Llamar a la función en el main y darle valores.
+
+    Segunda parte:
+
+    Crear una clase coche.
+
+    Dentro de la clase coche, una variable numérica que almacene el número de puertas que tiene.
+
+    Una función que incremente el número de puertas que tiene el coche.
+
+    Crear un objeto miCoche en el main y añadirle una puerta.
+
+    Mostrar el número de puertas que tiene el objeto.
+     */
+    
+    
+    public static void main(String[] args) {
+        suma(10, 15, 20);
+
+        Coche miCoche = new Coche();
+        miCoche.incrementarPuertas();
+        System.out.println(miCoche.puertas);
+    }
+
+    public static void suma(int a, int b, int c) {
+        int resultado = a + b + c;
+        System.out.println(resultado);
+    }
+}
+
+class Coche {
+
+    public int puertas = 4;
+
+    public void incrementarPuertas() {
+        this.puertas++;
+    }
+}
