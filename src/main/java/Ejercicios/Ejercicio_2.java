78b092eaf0dec4c2f8fcc877c1c904c1dcca1794
Andy <ander1386@gmail.com>
2022-10-06 09:02:33 -0500

    exercise 2.1

---
+++ src/main/java/Ejercicios/Ejercicio_2.java
@@ -0,0 +1,17 @@
+package Ejercicios;
+
+
+public class Ejercicio_2 {
+	
+	public static void main(String[] args) {
+		int numeroIf = 5;
+		
+		if (numeroIf > 0) {
+			System.out.println("El numero es positivo");
+		} else if (numeroIf < 0) {
+			System.out.println("El numero es negativo");
+		} else {
+			System.out.println("El numero es 0");
+		}
+	}
+}
