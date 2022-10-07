18095e7d6516046c838eddbe7936d43f7c20c7b6
Andy <ander1386@gmail.com>
2022-10-07 09:36:01 -0500

    exercise 3.2

--- src/main/java/Ejercicios/Ejercicio_3.java
+++ src/main/java/Ejercicios/Ejercicio_3.java
@@ -1,5 +1,19 @@
 package Ejercicios;
 
+/*
+Crear clase Persona.
+Crear variables las privadas edad, nombre y telefono de la clase Persona.	
+Crear gets y sets de cada propiedad.	
+Crear un objeto persona en el main.	
+Utiliza los gets y sets para darle valores a las propiedades edad, nombre y telefono, por último muéstralas por consola.
+  */
+
 public class Ejercicio_3 {
 
+}
+
+class Persona {
+	private int edad;
+	private String nombre;
+	private String telefono;
 }
