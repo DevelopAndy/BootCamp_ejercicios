667dd29a896e92067bb298f70c98cc179613d289
Andy <ander1386@gmail.com>
2022-10-12 08:36:03 -0500

    exercise 3.1

--- src/main/java/Python/Ejercicio_3.py
+++ src/main/java/Python/Ejercicio_3.py
@@ -2,3 +2,6 @@
 # calcule el índice de masa corporal y lo almacene en una variable, e imprima por pantalla 
 # la frase "Tu índice de masa corporal es" donde es el índice de masa corporal calculado 
 # redondeado con dos decimales.
+
+peso = float(input("Digite su peso en kg: "))
+estatura = float(input("Digite su estatura en metros: "))
