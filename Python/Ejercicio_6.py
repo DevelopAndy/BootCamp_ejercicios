e401bfda1c0ec12dada37e11a3c3ef51aca1b0d8
Andy <ander1386@gmail.com>
2022-10-14 15:58:43 -0500

    exercise 6.3 finished

--- Python/Ejercicio_6.py
+++ Python/Ejercicio_6.py
@@ -17,3 +17,11 @@ class Coche(Vehiculo):
     
     velocidad = 50
     cilindrada = 80
+
+c1 = Coche()
+c1.color = "violeta"
+print(f"El color del coche es: {c1.color}")
+print(f"El coche tiene: {c1.ruedas} ruedas")
+print(f"El coche tiene {c1.puertas} puertas")
+print(f"El coche tiene una velocidad de {c1.velocidad} km/h")
+print(f"El coche tiene un cilindraje de: {c1.cilindrada} cc")
