27ad7c3fd1adf7a6a5a833b7d7aab5c40870762d
Andy <ander1386@gmail.com>
2022-10-19 08:38:41 -0500

    importing 'ejercicio_7' module

---
+++ Python/main.py
@@ -0,0 +1,12 @@
+import Ejercicio_7 as o 
+
+def main():
+    suma = o.sumar(3,2)
+    resta = o.restar(3, 2)
+    multiplicacion = o.multiplicar (3, 2)
+    division = o.dividir(3, 2)
+    
+    print(f"la suma es: {suma} \nla resta es: {resta} \nla multiplicación es: {multiplicacion} \nla división es: {division}")
+   
+if __name__ == "__main__":
+    main() 
