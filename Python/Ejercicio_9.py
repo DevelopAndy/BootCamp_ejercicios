48ea915a97e388f56e7f75bf328bde658537411f
Andy <ander1386@gmail.com>
2022-11-02 16:11:35 -0500

    execirse 9.1

--- Python/Ejercicio_9.py
+++ Python/Ejercicio_9.py
@@ -1,3 +1,7 @@
 # En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, 
 # lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos 
 # veces al archivo creado.
+
+f = open('ejercicio_9.txt', 'w')
+f.write("ejercicio 9")
+f.close()
