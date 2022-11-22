0d53197bf01eedf55e5dfcded80ea81f5b70aa20
Andy <ander1386@gmail.com>
2022-11-22 10:37:30 -0500

    Exercise 10 finished

--- Python/Ejercicio_10.py
+++ Python/Ejercicio_10.py
@@ -2,3 +2,17 @@
 # Éstos se deben almacenar en una lista. No debería haber países repetidos (haz uso de set). 
 # Finalmente, muestra por consola la lista de países ordenados alfabéticamente y separados 
 # por comas.
+
+paises = input("Escriba una lista de países separados por coma (,): ")
+
+lista = set(paises.split(","))
+
+listaPaises = list(lista)
+listaPaisesSinEspacios = []
+
+for i in listaPaises:
+    listaPaisesSinEspacios.append(i.lstrip())
+
+listaPaisesSinEspacios.sort()
+
+print(listaPaisesSinEspacios)
