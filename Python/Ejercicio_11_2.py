a5ba6740ec347346f19e94121b165b05a0d09f27
Andy <ander1386@gmail.com>
2022-11-23 12:14:22 -0500

    exercise 11.2 finished

--- Python/Ejercicio_11_2.py
+++ Python/Ejercicio_11_2.py
@@ -2,3 +2,26 @@
 # de contener una lista de elementos seleccionables, también debe de tener un label 
 # con el texto que queráis.
 
+import tkinter
+from tkinter import ttk
+
+window = tkinter.Tk()
+
+window.columnconfigure(0, weight=1)
+window.columnconfigure(1, weight=3)
+
+selected = tkinter.StringVar()
+
+r1 = ttk.Radiobutton(window, text='Sí', value='1', variable=selected)
+r2 = ttk.Radiobutton(window, text='No', value='2', variable=selected)
+r3 = ttk.Radiobutton(window, text='Quizás', value='3', variable=selected)
+
+r1.grid(column=0, row=1, padx=5, pady=5)
+r2.grid(column=0, row=2, padx=5, pady=5)
+r3.grid(column=0, row=3, padx=5, pady=5)
+
+label = ttk.Label(window, text="Opciones:")
+label.grid(column=0, row=0, sticky=tkinter.W, padx=5, pady=5)
+
+
+window.mainloop()
