472633bb876bcd7853432dd748c9363b24fdf6a7
Andy <ander1386@gmail.com>
2022-12-13 17:44:21 -0500

    Exercise 1 finished

--- Java/src/Ejercicios/Ejercicio1.java
+++ Java/src/Ejercicios/Ejercicio1.java
@@ -1,4 +1,21 @@
+/* Para este primer ejercicio tendréis que realizar lo siguiente:
+        Crea un proyecto de Java desde 0
+        Dentro del proyecto tenéis que crear un paquete. En el paquete tendréis que crear una
+        clase.
+        Dentro de la clase tenéis que crear el método main e imprimir todos los datos que se
+        han visto en las sesiones.
+        Recordatorio: Los tipos de datos más comunes son int, long, double, boolean, String. */
+
 package Ejercicios;
 
 public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+        int numeroEntero = 1;
+        long numeroEnteroLargo = 2;
+        double numeroDecimal = 1.5d;
+        boolean falsoVerdadero = true;
+        String cadenaTexto = "Esto es una cadena de texto";
+    }
 }
