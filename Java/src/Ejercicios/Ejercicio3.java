b29ce30216337284ff40557890a6b310ef20790b
Andy <ander1386@gmail.com>
2022-12-14 11:07:55 -0500

    Exercise 3

---
+++ Java/src/Ejercicios/Ejercicio3.java
@@ -0,0 +1,11 @@
+/* En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final
+    por consola. Tened en cuenta que los textos pueden venir de un array de tipo String.*/
+
+package Ejercicios;
+
+public class Ejercicio3 {
+
+    public static void main(String[] args) {
+
+    }
+}
