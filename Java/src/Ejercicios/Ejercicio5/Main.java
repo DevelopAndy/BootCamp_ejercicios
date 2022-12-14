11e7d67ef96e8a7ca5e9d1c4f538a3de5279f5fd
Andy <ander1386@gmail.com>
2022-12-14 17:48:04 -0500

    Exercise 5 changing object to CocheCRUD

--- Java/src/Ejercicios/Ejercicio5/Main.java
+++ Java/src/Ejercicios/Ejercicio5/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        CocheCRUDImpl coche1 = new CocheCRUDImpl();
+        CocheCRUD coche1 = new CocheCRUDImpl();
 
         coche1.save();
         coche1.findAll();
