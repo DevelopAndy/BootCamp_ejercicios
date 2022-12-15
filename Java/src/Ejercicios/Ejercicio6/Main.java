77bc9f1eeea403c8600a3bc8b0b195af320a33ba
Andy <ander1386@gmail.com>
2022-12-15 17:12:38 -0500

    exercise 6.2

--- Java/src/Ejercicios/Ejercicio6/Main.java
+++ Java/src/Ejercicios/Ejercicio6/Main.java
@@ -3,10 +3,18 @@ package Ejercicios.Ejercicio6;
 public class Main {
 
     public static void main(String[] args) {
-
+        /* Escribe el código que devuelva una cadena al revés. Por ejemplo, la cadena "hola mundo",
+        debe retornar "odnum aloh".*/
         String texto = "hola mundo";
         String textoInvertido = reverse(texto);
         System.out.println(textoInvertido);
+
+        /* Crea un array unidimensional de Strings y recórrelo, mostrando únicamente sus valores.*/
+        String nombres[] = {"Carlos", "Andres", "Francisco", "Mireya", "Juan"};
+
+        for(String nombre:nombres) {
+            System.out.println(nombre);
+        }
 
     }
 
