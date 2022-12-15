d191d30f92ca71bbeec0eab5966da0a934247861
Andy <ander1386@gmail.com>
2022-12-15 17:15:57 -0500

    exercise 6.3

--- Java/src/Ejercicios/Ejercicio6/Main.java
+++ Java/src/Ejercicios/Ejercicio6/Main.java
@@ -16,6 +16,15 @@ public class Main {
             System.out.println(nombre);
         }
 
+        /*Crea un array bidimensional de enteros y recórrelo, mostrando la posición y el valor de
+        cada elemento en ambas dimensiones
+         */
+        int enteros[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        for(int i=0; i<enteros.length; i++) {
+            System.out.println("La posición es: " + i + " y el valor es: " + enteros[i]);
+        }
+
     }
 
     public static String reverse(String texto) {
