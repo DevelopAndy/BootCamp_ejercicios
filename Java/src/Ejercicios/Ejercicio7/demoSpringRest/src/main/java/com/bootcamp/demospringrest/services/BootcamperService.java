4cd964e45a3b9d3a66683a0a9c49f73c992f552c
Andy <ander1386@gmail.com>
2022-12-20 22:08:59 -0500

    Services

---
+++ Java/src/Ejercicios/Ejercicio7/demoSpringRest/src/main/java/com/bootcamp/demospringrest/services/BootcamperService.java
@@ -0,0 +1,29 @@
+package com.bootcamp.demospringrest.services;
+
+import com.bootcamp.demospringrest.models.Bootcamper;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BootcamperService {
+    private List<Bootcamper> bootcampers = new ArrayList();
+
+    public List<Bootcamper> getAll() {
+        return bootcampers;
+    }
+
+    public void add(Bootcamper bootcamper) {
+        bootcampers.add(bootcamper);
+    }
+
+    public Bootcamper get(String nombre) {
+        for (Bootcamper bootcamper:bootcampers) {
+            if(bootcamper.getNombre().equalsIgnoreCase(nombre)) {
+                return bootcamper;
+            }
+        }
+        return null;
+    }
+}
