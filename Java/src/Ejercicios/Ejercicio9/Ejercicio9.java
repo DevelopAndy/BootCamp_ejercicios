ce66a3c3b19a4a112b8ce1ee8bd6fdaa87e26d5b
Andy <ander1386@gmail.com>
2022-12-23 18:19:45 -0500

    Exercise 9 finished

--- Java/src/Ejercicios/Ejercicio9/Ejercicio9.java
+++ Java/src/Ejercicios/Ejercicio9/Ejercicio9.java
@@ -1,4 +1,25 @@
 package Ejercicios.Ejercicio9;
 
 public class Ejercicio9 {
+    private static Ejercicio9 ejercicio9;
+    int contador;
+
+    private Ejercicio9() {
+
+    }
+
+    public static Ejercicio9 getInstance() {
+        if(ejercicio9 == null) {
+            ejercicio9 = new Ejercicio9();
+        }
+        return ejercicio9;
+    }
+
+    public int getContador() {
+        return contador;
+    }
+
+    public void setContador(int contador) {
+        this.contador = contador;
+    }
 }
--- Java/src/Ejercicios/Ejercicio9/Main.java
+++ Java/src/Ejercicios/Ejercicio9/Main.java
@@ -1,4 +1,11 @@
 package Ejercicios.Ejercicio9;
 
 public class Main {
+    public static void main(String[] args) {
+        Ejercicio9 ej1 = Ejercicio9.getInstance();
+        Ejercicio9 ej2 = Ejercicio9.getInstance();
+
+        System.out.println(ej1);
+        System.out.println(ej2);
+    }
 }
