2215277057be290d7e5fc0cf578a3b1049dbb7ce
Andy <ander1386@gmail.com>
2022-12-28 19:17:37 -0500

    class coche

--- Java/src/Ejercicios/Ejercicio10/Coche.java
+++ Java/src/Ejercicios/Ejercicio10/Coche.java
@@ -1,4 +1,23 @@
 package Ejercicios.Ejercicio10;
 
 public class Coche {
+    private String color;
+    private String marca;
+
+    private Coche() {
+
+    }
+
+    public Coche(String color, String marca) {
+        this.color = color;
+        this.marca = marca;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
 }
