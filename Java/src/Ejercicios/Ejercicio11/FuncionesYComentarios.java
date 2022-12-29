3a6237d042a4e262c5008efb654843a95ee1a81d
Andy <ander1386@gmail.com>
2022-12-29 18:27:19 -0500

    Comments

--- Java/src/Ejercicios/Ejercicio11/FuncionesYComentarios.java
+++ Java/src/Ejercicios/Ejercicio11/FuncionesYComentarios.java
@@ -6,6 +6,7 @@ public class FuncionesYComentarios {
 
     public static void main(String[] args) {
 
+        // sumar los numeros dados en los parametros de la unción
         int suma = sumarNumeros(1,2,3,4,5);
 
         // Lista de nombres
@@ -15,13 +16,19 @@ public class FuncionesYComentarios {
         nombres.add("Vanesa");
         nombres.add("Rodrigo");
 
+        // Imprimimos el resultado de la suma
         System.out.println(suma);
-
+        // Imprimimos falso o verdadero dependiendo de si existe el nombre
         System.out.println(existeNombre(nombres, "Andy"));
         System.out.println(existeNombre(nombres, "Alfons"));
 
     }
 
+    /**
+     * suma numeros
+     * @param numeros numeros para sumar dados por parametros
+     * @return devuelve elresultado de la suma
+     */
     public static int sumarNumeros(int ...numeros) {
         int resultado = 0;
 
@@ -32,6 +39,12 @@ public class FuncionesYComentarios {
         return resultado;
     }
 
+    /**
+     * Verifica siun nombre existe en un Array
+     * @param nombres uno o varios objetos de tipo String
+     * @param nombreABuscar el nombre que se va a buscar en el array
+     * @return devuelve verdadero si existe elusuario y falso si no
+     */
     public static boolean existeNombre(ArrayList nombres, String nombreABuscar) {
         return nombres.contains(nombreABuscar);
     }
