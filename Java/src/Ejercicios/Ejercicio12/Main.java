020e343a0ac5daedac3b5942413eb007650203b7
Andy <ander1386@gmail.com>
2023-01-10 09:18:58 -0500

    Exercise 12.2

--- Java/src/Ejercicios/Ejercicio12/Main.java
+++ Java/src/Ejercicios/Ejercicio12/Main.java
@@ -1,9 +1,19 @@
 package Ejercicios.Ejercicio12;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         int dividendo = 5;
         int divisor = 0;
+
+        ArrayList<Integer> numeros = new ArrayList<>();
+        numeros.add(1);
+        numeros.add(2);
+        numeros.add(3);
+        numeros.add(4);
+        numeros.add(5);
+        numeros.add(6);
 
         try {
             int division = dividendo / divisor;
@@ -11,6 +21,15 @@ public class Main {
         }
         catch (ArithmeticException e) {
             System.out.println("No se puede dividir: " + e.getMessage());
+        }
+
+        try {
+            for(int i=0; i<=9; i++) {
+                System.out.println(numeros.get(i));
+            }
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("El array tiene menos elementos " + e.getMessage());
         }
     }
 }
