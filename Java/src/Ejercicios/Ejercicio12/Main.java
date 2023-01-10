f4f5dcee41f5f2a02f394491b5e450082ed7e1f9
Andy <ander1386@gmail.com>
2023-01-10 09:07:14 -0500

    Exercise 12.1

--- Java/src/Ejercicios/Ejercicio12/Main.java
+++ Java/src/Ejercicios/Ejercicio12/Main.java
@@ -1,4 +1,16 @@
 package Ejercicios.Ejercicio12;
 
 public class Main {
+    public static void main(String[] args) {
+        int dividendo = 5;
+        int divisor = 0;
+
+        try {
+            int division = dividendo / divisor;
+            System.out.println(division);
+        }
+        catch (ArithmeticException e) {
+            System.out.println("No se puede dividir: " + e.getMessage());
+        }
+    }
 }
