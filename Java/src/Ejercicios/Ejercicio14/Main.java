c05a25808a4cc582b0bbdfb14b0e7e3cbc1626ae
Andy <ander1386@gmail.com>
2023-01-13 11:06:28 -0500

    Exercise 14 finished

--- Java/src/Ejercicios/Ejercicio14/Main.java
+++ Java/src/Ejercicios/Ejercicio14/Main.java
@@ -7,12 +7,10 @@ public class Main {
         Coche carro = new VehiculoElectrico(80, "carro", true);
 
         System.out.println("\n Moto:\n");
-
         System.out.println(moto.getTipo());
         System.out.println(moto.getVelocidad());
 
         System.out.println("\n Electrico:\n");
-
         System.out.println(carro.getTipo());
         System.out.println(carro.getVelocidad());
         System.out.println(carro.getBaterias());
