911f9428aa07414a976468d8e36fe2ae1f54657f
Andy <ander1386@gmail.com>
2023-01-13 09:57:40 -0500

    Exercise 14.1

--- Java/src/Ejercicios/Ejercicio14/Coche.java
+++ Java/src/Ejercicios/Ejercicio14/Coche.java
@@ -1,4 +1,18 @@
 package Ejercicios.Ejercicio14;
 
 public class Coche {
+
+    private int velocidad;
+
+    public void acelerar() {
+        velocidad++;
+    }
+
+    public void frenar() {
+        velocidad--;
+    }
+
+    public int getVelocidad() {
+        return velocidad;
+    }
 }
