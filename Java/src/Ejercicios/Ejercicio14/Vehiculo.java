c7ebcdfa4130a26814f3b9dadbe51bfd434c9efb
Andy <ander1386@gmail.com>
2023-01-14 11:38:21 -0500

    Exercise 15.4

--- Java/src/Ejercicios/Ejercicio14/Vehiculo.java
+++ Java/src/Ejercicios/Ejercicio14/Vehiculo.java
@@ -1,9 +1,32 @@
 package Ejercicios.Ejercicio14;
 
-public class Vehiculo extends Coche{
+public class Vehiculo implements Coche{
+
+    private int velocidad;
+    private String tipo;
 
     public Vehiculo(int velocidad, String tipo) {
         this.velocidad = velocidad;
         this.tipo = tipo;
+    }
+
+    @Override
+    public void acelerar() {
+        velocidad++;
+    }
+
+    @Override
+    public void frenar() {
+        velocidad--;
+    }
+
+    @Override
+    public int getVelocidad() {
+        return velocidad;
+    }
+
+    @Override
+    public String getTipo() {
+        return tipo;
     }
 }
