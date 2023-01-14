55145916b6f88623275861bf4bffcbf1f8e4a33b
Andy <ander1386@gmail.com>
2023-01-14 11:58:33 -0500

    Exercise 15.9

--- Java/src/Ejercicios/Ejercicio14/Main.java
+++ Java/src/Ejercicios/Ejercicio14/Main.java
@@ -3,7 +3,7 @@ package Ejercicios.Ejercicio14;
 public class Main {
 
     public static void main(String[] args) {
-        Coche moto = new Vehiculo(50, "moto");
+        Vehiculo moto = new Vehiculo(50, "moto");
         VehiculoElectrico carro = new VehiculoElectrico(80, "carro", true);
 
         System.out.println("\n Moto:\n");
@@ -15,5 +15,12 @@ public class Main {
         System.out.println(carro.getVelocidad());
         System.out.println(carro.getBaterias());
 
+        DbMySQL db = new DbMySQL();
+        guardarEnBaseDeDatos(db, moto);
+
+    }
+
+    public static void guardarEnBaseDeDatos(DbStore dbs, Vehiculo vehiculo) {
+        dbs.guardar(vehiculo);
     }
 }
